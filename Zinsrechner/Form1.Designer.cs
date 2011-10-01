661e8afff39f1200c34528675082f380a44bd86d
pyto <famkalp@online.de>
2011-10-01 08:30:34 +0200

    Hier kommt ein Zinsrechner - noch uncompiliert
    Signed-off-by: pyto <famkalp@online.de>

---
+++ Zinsrechner/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Zinsrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.ZPJ = new System.Windows.Forms.TabPage();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ZPM = new System.Windows.Forms.TabPage();
+            this.date = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.ZPJ.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.ZPJ);
+            this.tabControl1.Controls.Add(this.ZPM);
+            this.tabControl1.Controls.Add(this.date);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(899, 336);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // ZPJ
+            // 
+            this.ZPJ.Controls.Add(this.maskedTextBox1);
+            this.ZPJ.Controls.Add(this.label3);
+            this.ZPJ.Controls.Add(this.label2);
+            this.ZPJ.Controls.Add(this.label1);
+            this.ZPJ.Controls.Add(this.textBox3);
+            this.ZPJ.Controls.Add(this.textBox1);
+            this.ZPJ.Location = new System.Drawing.Point(4, 22);
+            this.ZPJ.Name = "ZPJ";
+            this.ZPJ.Padding = new System.Windows.Forms.Padding(3);
+            this.ZPJ.Size = new System.Drawing.Size(891, 310);
+            this.ZPJ.TabIndex = 0;
+            this.ZPJ.Text = "Zinsen pro Jahr";
+            this.ZPJ.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(383, 21);
+            this.maskedTextBox1.Mask = "000,00%";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(294, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Zinssatz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(558, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Auszahlung:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Einzahlung:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(674, 21);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(116, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ZPM
+            // 
+            this.ZPM.Location = new System.Drawing.Point(4, 22);
+            this.ZPM.Name = "ZPM";
+            this.ZPM.Padding = new System.Windows.Forms.Padding(3);
+            this.ZPM.Size = new System.Drawing.Size(891, 310);
+            this.ZPM.TabIndex = 1;
+            this.ZPM.Text = "Zinsen pro Monat";
+            this.ZPM.UseVisualStyleBackColor = true;
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(4, 22);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(891, 310);
+            this.date.TabIndex = 2;
+            this.date.Text = "Datumsrechner";
+            this.date.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 360);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ZPJ.ResumeLayout(false);
+            this.ZPJ.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage ZPJ;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage ZPM;
+        private System.Windows.Forms.TabPage date;
+    }
+}
+
